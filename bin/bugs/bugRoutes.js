de73c5a7e27e583f09770f6ffc7740c6d8407212
JG <joshgundy55@gmail.com>
2021-07-28 13:51:13 -0500

    Implement separation layer boilerplate

--- bin/bugs/bugRoutes.js
+++ bin/bugs/bugRoutes.js
@@ -3,15 +3,34 @@ const bugControl = require('./bugController');
 
 const router = express.Router();
 
+// TODO: implement extraction functions
+// to ensure a separation of our web layer
+// from our controller
+
 // GET a list of all bugs
-router.get('/bugs', bugControl.list);
+router.get('/bugs', (req, res) => {
+  const parse = [...req];
+  bugControl.list(parse);
+});
 // GET a specific bug
-router.get('/bugs/:id', bugControl.detail);
+router.get('/bugs/:id', (req, res) => {
+  const parse = [...req];
+  bugControl.detail(parse);
+});
 // CREATE a new bug
-router.post('/bugs/:id', bugControl.create);
+router.post('/bugs/:id', (req, res) => {
+  const parse = [...req];
+  bugControl.create(parse);
+});
 // DELETE a bug
-router.delete('/bugs/:id', bugControl.remove);
+router.delete('/bugs/:id', (req, res) => {
+  const parse = [...req];
+  bugControl.remove(parse);
+});
 // UPDATE a bug
-router.put('/bugs/:id', bugControl.update);
+router.put('/bugs/:id', (req, res) => {
+  const parse = [...req];
+  bugControl.update(parse);
+});
 
 module.exports = router;
