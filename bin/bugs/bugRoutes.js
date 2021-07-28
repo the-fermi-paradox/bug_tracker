9409d1c76c483bd060dc0d919e09a9838198ebd8
JG <joshgundy55@gmail.com>
2021-07-28 15:08:13 -0500

    Extract id from request and send it to our controllers

--- bin/bugs/bugRoutes.js
+++ bin/bugs/bugRoutes.js
@@ -3,38 +3,33 @@ const bugControl = require('./bugController');
 
 const router = express.Router();
 
-// TODO: implement extraction functions
-// to ensure a separation of our web layer
-// from our controller
-
 // GET a list of all bugs
-router.get('/bugs', (req, res) => {
-  const parse = [...req];
-  bugControl.list(parse);
+router.get('/bugs', () => {
+  bugControl.list();
 });
 
 // GET a specific bug
-router.get('/bugs/:id', (req, res) => {
-  const parse = [...req];
-  bugControl.detail(parse);
+router.get('/bugs/:id', (req) => {
+  const { id } = req.params;
+  bugControl.detail(id);
 });
 
 // CREATE a new bug
-router.post('/bugs/:id', (req, res) => {
-  const parse = [...req];
-  bugControl.create(parse);
+router.post('/bugs/:id', (req) => {
+  const { id } = req.params;
+  bugControl.create(id);
 });
 
 // DELETE a bug
-router.delete('/bugs/:id', (req, res) => {
-  const parse = [...req];
-  bugControl.delete(parse);
+router.delete('/bugs/:id', (req) => {
+  const { id } = req.params;
+  bugControl.delete(id);
 });
 
 // UPDATE a bug
-router.put('/bugs/:id', (req, res) => {
-  const parse = [...req];
-  bugControl.update(parse);
+router.put('/bugs/:id', (req) => {
+  const { id } = req.params;
+  bugControl.update(id);
 });
 
 module.exports = router;
