1012301fd6defa80c6927c509a160edc56ac02da
JG <joshgundy55@gmail.com>
2021-08-03 18:17:12 -0500

    Debugging invalid put request

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -2,6 +2,7 @@ const processJoiError = require('../errors/process_joi_error');
 
 const validate = (schema) => (req, res, next) => {
   const { error } = schema.validate(req.body);
+  console.log(error);
   if (error != null) {
     next();
   } else {
