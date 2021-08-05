4d09789493ad8ccc7772068cb3aabe1d7c8262ef
JG <joshgundy55@gmail.com>
2021-08-05 13:31:17 -0500

    Fix bug

--- database.js
+++ database.js
@@ -12,12 +12,15 @@ class Database {
   }
 
   async ask(query, params) {
-    let logString = '';
+    // Let's build a string so we can log it to stdout
+    let logString = query;
     for (let i = 0; i < params.length; i += 1) {
       const val = params[i];
-      logString = query.replace('?', val);
+      logString = logString.replace('?', val);
     }
     logger.info(logString);
+    // We don't actually use this string though - we use the query with proper
+    // parameters to prevent SQL injection
     const data = await this.pool.query(query, params);
     logger.info(data);
     return data;
