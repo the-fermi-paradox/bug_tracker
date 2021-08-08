7fda98570ce70a3c450e8ed0b9ac63837837b9c3
JG <joshgundy55@gmail.com>
2021-08-07 19:13:43 -0500

    Bug fixing

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -3,7 +3,6 @@ const processJoiError = require('../errors/process_joi_error');
 const validate = (schema) => (req, res, next) => {
   console.log(req.body);
   const { value, error } = schema.validate(req.body);
-  console.log(error);
   console.log(value);
   error != null ? next(processJoiError(error)) : next();
 };
